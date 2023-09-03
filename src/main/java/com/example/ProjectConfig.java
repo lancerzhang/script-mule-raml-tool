58f154c55279be094a03b97c19aef7df10266ee4
lancerzhang <lancerz@hotmail.com>
2023-09-03 23:10:41 +0800

    read RAML

---
+++ src/main/java/com/example/ProjectConfig.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class ProjectConfig {
+    private String projectPath;
+
+    // Getters and setters
+    public String getProjectPath() {
+        return projectPath;
+    }
+
+    public void setProjectPath(String projectPath) {
+        this.projectPath = projectPath;
+    }
+}
--- src/main/java/com/example/ScriptMuleRamlToolApplication.java
+++ src/main/java/com/example/ScriptMuleRamlToolApplication.java
@@ -1,55 +1,57 @@
 package com.example;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.raml.v2.api.RamlModelBuilder;
+import org.raml.v2.api.RamlModelResult;
+import org.raml.v2.api.model.v08.api.Api;
+import org.raml.v2.api.model.v08.methods.Method;
+import org.raml.v2.api.model.v08.resources.Resource;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.FileVisitOption;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.SimpleFileVisitor;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.EnumSet;
+import java.util.List;
 
 @SpringBootApplication
 public class ScriptMuleRamlToolApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ScriptMuleRamlToolApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ScriptMuleRamlToolApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args) {
-		Path startingDir = Paths.get("combined_flow_xml");
-		try {
-			Files.walkFileTree(startingDir, EnumSet.noneOf(FileVisitOption.class), Integer.MAX_VALUE, new SimpleFileVisitor<Path>() {
-				@Override
-				public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
-					if (file.getFileName().toString().equals("java_classes.txt")) {
-						readAndPrintFile(file.toFile());
-					}
-					return FileVisitResult.CONTINUE;
-				}
-			});
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    @Override
+    public void run(String... args) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        ProjectConfig config = mapper.readValue(new File("config.json"), ProjectConfig.class);
 
-	private void readAndPrintFile(File file) {
-		try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-			String line;
-			while ((line = reader.readLine()) != null) {
-				System.out.println("FQCN: " + line);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+        String apiDirPath = config.getProjectPath() + "/src/main/api";
+        File apiDir = new File(apiDirPath);
+
+        File[] ramlFiles = apiDir.listFiles((dir, name) -> name.endsWith(".raml"));
+        if (ramlFiles != null && ramlFiles.length == 1) {
+            String ramlFilePath = ramlFiles[0].getAbsolutePath();
+            RamlModelResult ramlModelResult = new RamlModelBuilder().buildApi(ramlFilePath);
+
+            if (ramlModelResult.hasErrors()) {
+                System.out.println("Errors in the RAML file");
+                return;
+            }
+
+            Api api = ramlModelResult.getApiV08();
+            List<Resource> resources = api.resources();
+            printEndpoints(resources, "");
+        }
+    }
+
+    private void printEndpoints(List<Resource> resources, String parentUrl) {
+        for (Resource resource : resources) {
+            String url = parentUrl + resource.relativeUri().value();
+            for (Method method : resource.methods()) {
+                System.out.println("Endpoint: " + url + ", Method: " + method.method());
+            }
+            printEndpoints(resource.resources(), url);
+        }
+    }
 }
